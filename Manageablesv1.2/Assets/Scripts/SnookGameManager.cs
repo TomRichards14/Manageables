5c3bec470fb05669d69e3a85e3ab0717d2ebdd0d
TomRichards14 <2richt38@solent.ac.uk>
2017-11-23 01:08:49 +0000

    Snook Started
    Needs latest files from Harry inc. RanchLevel scene, Match2 scenes and scripts

---
+++ Manageablesv1.2/Assets/Scripts/SnookGameManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SnookGameManager : MonoBehaviour {
+
+    public GameObject snakePrefab;
+    public GameObject itemPrefab;
+    public bool ItemAvailable;
+
+	// Use this for initialization
+	void Start ()
+    {
+        ItemAvailable = false;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (ItemAvailable == false)
+        {
+            ItemSpawn();
+        }
+	}
+
+    void ItemSpawn()
+    {
+        float XCoord;
+        float YCoord;
+
+        XCoord = Random.Range(-3.0f, 3.0f);
+        YCoord = Random.Range(-4.4f, 6.4f);
+
+        Instantiate(itemPrefab, new Vector3(XCoord, YCoord, -0.15f), Quaternion.identity);
+
+        ItemAvailable = true;
+    }
+}
---
+++ Manageablesv1.2/Assets/Scripts/SnookMovement.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SnookMovement : MonoBehaviour {
+
+    public int DirectionOfTravel;
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		if (Input.GetKey(KeyCode.W))
+        {
+            DirectionOfTravel = 1;
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            DirectionOfTravel = 2;
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            DirectionOfTravel = 3;
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            DirectionOfTravel = 4;
+        }
+
+        switch (DirectionOfTravel)
+        {
+            case 1:
+                if (transform.position.y < 6.4f)
+                    transform.Translate(Vector3.up * Time.deltaTime);
+                break;
+            case 2:
+                if (transform.position.y > -4.4f)
+                    transform.Translate(Vector3.down * Time.deltaTime);
+                break;
+            case 3:
+                if (transform.position.x > -3.0f)
+                    transform.Translate(Vector3.left * Time.deltaTime);
+                break;
+            case 4:
+                if (transform.position.x < 3.0f)
+                    transform.Translate(Vector3.right * Time.deltaTime);
+                break;
+        }
+	}
+}
