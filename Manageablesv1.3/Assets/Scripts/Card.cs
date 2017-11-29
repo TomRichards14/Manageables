385ec9e05526c9645af748d7a12da717bbc60764
TomRichards14 <2richt38@solent.ac.uk>
2017-11-29 01:34:10 +0000

    Contains basic snake movement
    Still needs merge with Harry's updated ranch scenes and scripts

---
+++ Manageablesv1.3/Assets/Scripts/Card.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Card : MonoBehaviour {
+
+    public static bool do_not = false; //Stops card flipping process
+
+    [SerializeField]
+    private int _state;
+    [SerializeField]
+    private int _cardValue;
+    [SerializeField]
+    private bool _initialized = false;
+
+    private Sprite _cardBack; //Back of Card
+    private Sprite _cardFace; //Face of card
+
+    private GameObject _manager; //Game Manager object
+
+    void Start()
+    {
+        _state = 1; //Sets state to 1
+        _manager = GameObject.FindGameObjectWithTag("Manager"); //Sets _manager to GameManager object
+
+    }
+
+    public void setupGraphics()
+    {
+        _cardBack = _manager.GetComponent<Match2GameManager>().getCardBack ();
+        _cardFace = _manager.GetComponent<Match2GameManager>().getCardFace (_cardValue);
+
+        flipCard(); //Calls flip card function
+    }
+
+    public void flipCard()
+    {
+
+        if (_state ==0)
+        {
+            _state = 1;
+        }
+        else if (_state == 1)
+        {
+            _state = 0;
+        }
+
+        if(_state == 0 && !do_not)
+            GetComponent<Image>().sprite = _cardBack;
+        else if (_state == 1 && !do_not)
+            GetComponent<Image>().sprite = _cardFace;
+    }
+
+    public int cardValue
+    {
+        get { return _cardValue;  }
+        set { _cardValue = value; }
+    }
+
+    public int state
+    {
+        get { return _state; }
+        set { _state = value; }
+    }
+    
+    public bool Initialized
+    {
+        get { return _initialized; }
+        set { _initialized = value; }
+    }
+
+    public void falseCheck()
+    {
+        StartCoroutine(pause ());
+    }
+    IEnumerator pause()
+    {
+        yield return new WaitForSeconds(1);
+        if (_state == 0)
+            GetComponent<Image>().sprite = _cardBack;
+        else if (_state == 1)
+            GetComponent<Image>().sprite = _cardFace;
+        do_not = false;
+    }
+}
---
+++ Manageablesv1.3/Assets/Scripts/SnookGameManager.cs
@@ -0,0 +1,102 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SnookGameManager : MonoBehaviour {
+
+    public List<Transform> SnakeBody = new List<Transform>();
+
+    public GameObject snakePrefab;
+    public GameObject itemPrefab;
+    public Transform CurrentSnakeBlock;
+    public Transform PreviousSnakeBlock;
+    public bool ItemAvailable;
+    public float BodyMinDistance = 0.1f;
+    public float SnakeRotationSpeed = 0.25f;
+    public int SnakeStartSize = 3;
+
+    private float DistanceBetweenBodyParts;
+
+	// Use this for initialization
+	void Start ()
+    {
+        for (int j = 0; j < SnakeStartSize; j++ )
+        {
+            AddToSnake();
+        }
+
+        ItemAvailable = false;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (ItemAvailable == false)
+        {
+            ItemSpawn();
+        }
+
+        SnakeMovement();
+
+        if (Input.GetKey(KeyCode.Q))
+        {
+            AddToSnake();
+        }
+	}
+
+    void ItemSpawn()
+    {
+        float XCoord;
+        float YCoord;
+
+        XCoord = Random.Range(-3.0f, 3.0f);
+        YCoord = Random.Range(-4.4f, 6.4f);
+
+        Instantiate(itemPrefab, new Vector3(XCoord, YCoord, -0.15f), Quaternion.identity);
+
+        ItemAvailable = true;
+    }
+
+    void SnakeMovement()
+    {
+        float SnakeSpeed = 1.0f;
+
+        SnakeBody[0].Translate(SnakeBody[0].forward * SnakeSpeed * Time.smoothDeltaTime);
+
+        if (Input.GetAxis("Horizontal") != 0)
+        {
+            SnakeBody[0].Rotate(Vector3.up * SnakeRotationSpeed * Time.deltaTime * Input.GetAxis("Horizontal"));
+        }
+
+        for (int i = 1; i < SnakeBody.Count; i++)
+        {
+            CurrentSnakeBlock = SnakeBody[i];
+            PreviousSnakeBlock = SnakeBody[i - 1];
+
+            DistanceBetweenBodyParts = Vector3.Distance(CurrentSnakeBlock.position, PreviousSnakeBlock.position);
+
+            Vector3 TargetLocation = PreviousSnakeBlock.position;
+
+            TargetLocation.y = SnakeBody[0].position.y;
+
+            float MoveToTargetSpeed = Time.deltaTime * DistanceBetweenBodyParts / BodyMinDistance * SnakeSpeed;
+
+            if (MoveToTargetSpeed > 0.5f)
+            {
+                MoveToTargetSpeed = 0.5f;
+            }
+
+            CurrentSnakeBlock.position = Vector3.Slerp(CurrentSnakeBlock.position, TargetLocation, MoveToTargetSpeed);
+            CurrentSnakeBlock.rotation = Quaternion.Slerp(CurrentSnakeBlock.rotation, PreviousSnakeBlock.rotation, MoveToTargetSpeed);
+        }
+    }
+
+    void AddToSnake()
+    {
+        Transform NewSnakeBlock = (Instantiate(snakePrefab, SnakeBody[SnakeBody.Count - 1].position, SnakeBody[SnakeBody.Count - 1].rotation) as GameObject).transform;
+
+        NewSnakeBlock.SetParent(transform);
+
+        SnakeBody.Add(NewSnakeBlock);
+    }
+}
